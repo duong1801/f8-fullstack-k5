71a70f610fecf37f58cb14eb9e8dacb4874ac5a6
duong1801 <duongx1801@gmail.com>
2024-03-11 12:43:58 +0700

    update code

--- Day-28/js/main.js
+++ Day-28/js/main.js
@@ -14,10 +14,13 @@ var indexActive = 0;
 var lastIndexActive = 0;
 var translateX = 0;
 var dotsHtmls = [];
+var isDrag = false;
+var potionsStartDrag = 0;
+var pullingDistance = 0;
 
 //Tính kích thước chiều rộng của 1 item
 var itemWidth = carouselImages.clientWidth;
-
+var distanceChange = (itemWidth / 100) * 15;
 //Tính tổng kích thước các item
 var totalWidth = itemWidth * carouselItems.length;
 
@@ -26,24 +29,27 @@ carouselImages.style.width = `${totalWidth}px`;
 
 for (var i = 0; i < carouselItems.length; i++) {
 	dotsHtmls.push(
-		`<span class='dot-item-${i}' onclick='handleActiveDots(${i})'></span>`
+		`<span class='dot-item-${i}' onclick='handleChangeSlide(${i})'></span>`
 	);
 }
 
 carouselDots.innerHTML = dotsHtmls.join("");
 
-handleActiveDots();
+handleChangeSlide();
 
-function handleActiveDots(index = 0) {
+function handleChangeSlide(index = 0) {
 	var dotActive = $(`.dot-item-${index}`);
 	var dotLastActive = $(`.dot-item-${lastIndexActive}`);
 	dotLastActive.classList.remove("active");
 	dotActive.classList.add("active");
-
-	translateX += (indexActive - index) * itemWidth;
-	carouselImages.style.translate = `${translateX}px`;
 	indexActive = index;
-	lastIndexActive = index;
+
+	translateX += (lastIndexActive - indexActive) * itemWidth;
+
+	// console.log(translateX);
+	console.log(lastIndexActive, indexActive);
+	carouselImages.style.translate = `${translateX}px`;
+	lastIndexActive = indexActive;
 }
 //Lắng nghe sự kiện click vào nút next
 
@@ -51,18 +57,56 @@ carouselNextBtn.addEventListener("click", function () {
 	if (Math.abs(translateX) >= totalWidth - itemWidth) {
 		return; //Thoát hàm
 	}
-	translateX -= itemWidth;
-	carouselImages.style.translate = `${translateX}px`;
+
 	indexActive++;
-	handleActiveDots(indexActive);
+	handleChangeSlide(indexActive);
 });
 
 carouselPrevBtn.addEventListener("click", function () {
 	if (Math.abs(translateX) < itemWidth) {
 		return; //Thoát hàm
 	}
-	translateX += itemWidth;
-	carouselImages.style.translate = `${translateX}px`;
+
 	indexActive--;
-	handleActiveDots(indexActive);
+	handleChangeSlide(indexActive);
+});
+
+carousel.addEventListener("mousedown", function (e) {
+	isDrag = true;
+	potionsStartDrag = e.clientX;
+});
+
+carousel.addEventListener("mousemove", function (e) {
+	e.preventDefault();
+	var maxPosition = itemWidth * carouselItems.length;
+	var minPosition = 0;
+
+	if (isDrag) {
+		pullingDistance = e.clientX - potionsStartDrag;
+
+		if (pullingDistance < 0) {
+			carouselImages.style.translate = `${translateX + pullingDistance}px`;
+			if (Math.abs(pullingDistance) > distanceChange) {
+				indexActive++;
+				isDrag = false;
+				handleChangeSlide(indexActive);
+			}
+		} else {
+			carouselImages.style.translate = `${translateX + pullingDistance}px`;
+			if (Math.abs(pullingDistance) > distanceChange) {
+				indexActive--;
+				isDrag = false;
+				handleChangeSlide(indexActive);
+			}
+		}
+	}
+});
+document.addEventListener("mousemove", function (e) {
+	e.preventDefault();
+	// e.stopPropagation;
+});
+
+carousel.addEventListener("mouseup", function (e) {
+	isDrag = false;
+	handleChangeSlide(indexActive);
 });
