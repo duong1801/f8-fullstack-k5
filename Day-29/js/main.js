c7212bb7a88d80e484c8a681f6e5ae5eb437df9b
duong1801 <duongx1801@gmail.com>
2024-03-14 16:31:10 +0700

    update code

---
+++ Day-29/js/main.js
@@ -0,0 +1,139 @@
+/** @format */
+
+var progressBar = document.querySelector(".progress-bar");
+var progress = progressBar.querySelector(".progress");
+var progressSpan = progress.querySelector("span");
+var timer = document.querySelector(".timer");
+var isClickedProgressBar = false;
+var isShowTimer = true;
+var progressBarWidth = progressBar.clientWidth;
+
+progressBar.addEventListener("mousedown", function (e) {
+	if (e.which === 1) {
+		timer.style.display = "none";
+		isClickedProgressBar = true;
+		var offsetX = e.offsetX;
+		var rate = (offsetX * 100) / progressBarWidth;
+		progress.style.width = `${rate}%`;
+		positionSpace = offsetX;
+		offsetWidth = offsetX;
+		initialClientX = e.clientX;
+		// audio.currentTime = (rate * audio.duration) / 100;
+		audio.removeEventListener("timeupdate", updatedTimer);
+		document.addEventListener("mousemove", handleDrag);
+	}
+});
+
+progressSpan.addEventListener("mousedown", function (e) {
+	e.stopPropagation();
+	isClickedProgressBar = true;
+
+	audio.removeEventListener("timeupdate", updatedTimer);
+	if (e.which === 1) {
+		document.addEventListener("mousemove", handleDrag);
+		initialClientX = e.clientX;
+	}
+});
+
+document.addEventListener("mouseup", function () {
+	if (isClickedProgressBar) {
+		document.removeEventListener("mousemove", handleDrag);
+		offsetWidth = positionSpace;
+		var rate = (offsetWidth / progressBarWidth) * 100;
+		audio.currentTime = (rate * audio.duration) / 100;
+		audio.addEventListener("timeupdate", updatedTimer);
+	}
+	isClickedProgressBar = false;
+});
+
+var initialClientX = 0;
+var offsetWidth = 0; //Khoảng cách ban đầu button so với progressBar
+var positionSpace = 0; //Khoảng cách kéo thêm tại vị trí ban đầu tới vị trí mới
+var handleDrag = function (e) {
+	timer.style.display = "none";
+	var clientX = e.clientX;
+
+	positionSpace = offsetWidth + (clientX - initialClientX);
+
+	var rate = (positionSpace * 100) / progressBarWidth;
+	if (rate < 0) {
+		rate = 0;
+	}
+	if (rate > 100) {
+		rate = 100;
+	}
+	progress.style.width = `${rate}%`;
+};
+
+//Xây dựng trình phát nhạc
+var audio = document.querySelector("audio");
+var durationEl = progressBar.nextElementSibling;
+var currentTimeEl = progressBar.previousElementSibling;
+var playBtn = document.querySelector(".player .player-action i");
+var getTime = function (seconds) {
+	var mins = Math.floor(seconds / 60);
+	seconds = Math.floor(seconds - mins * 60);
+	return `${mins < 10 ? "0" + mins : mins}:${
+		seconds < 10 ? "0" + seconds : seconds
+	}`;
+};
+
+//Lắng nghe sự kiện khi file mp3 được tải xong và trình duyệt lấy được thông tin
+durationEl.innerText = getTime(audio.duration);
+audio.addEventListener("loadeddata", function () {
+	durationEl.innerText = getTime(audio.duration);
+});
+
+//Khi người dùng click vào nút play
+playBtn.addEventListener("click", function () {
+	//Nếu nhạc đang dừng --> Phát nhạc
+	//Nếu nhạc đang chạy --> Dừng nhạc
+	if (audio.paused) {
+		audio.play();
+		this.classList.remove("fa-play");
+		this.classList.add("fa-pause");
+	} else {
+		audio.pause();
+		this.classList.remove("fa-pause");
+		this.classList.add("fa-play");
+	}
+});
+
+var updatedTimer = function () {
+	currentTimeEl.innerText = getTime(audio.currentTime);
+	//Tính tỷ lệ phần trăm
+	var rate = (audio.currentTime / audio.duration) * 100;
+	offsetWidth = (progressBarWidth / 100) * rate;
+	progress.style.width = `${rate}%`;
+};
+
+//Khi nhạc đang phát
+audio.addEventListener("timeupdate", updatedTimer);
+
+audio.addEventListener("ended", function () {
+	audio.currentTime = 0;
+	playBtn.classList.remove("fa-pause");
+	playBtn.classList.add("fa-play");
+	progress.style.width = "0%";
+	currentTimeEl.innerText = getTime(audio.currentTime);
+});
+
+var handleShowTimer = function (e) {
+	var offsetX = e.offsetX;
+	var rate = (offsetX * 100) / progressBarWidth;
+	var timing = (audio.duration * rate) / 100;
+	timer.style.left = `${rate}%`;
+	timer.innerText = getTime(timing);
+	timer.style.display = "block";
+};
+
+progressBar.addEventListener("mousemove", handleShowTimer);
+
+progressBar.addEventListener("mouseleave", function () {
+	timer.style.display = "none";
+});
+
+progressSpan.addEventListener("mousemove", function (e) {
+	e.stopPropagation();
+	timer.style.display = "none";
+});
