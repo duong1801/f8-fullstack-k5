ab2f3f9d351cad1df530f497cff3476bda624d10
duong1801 <duongx1801@gmail.com>
2024-04-05 14:40:51 +0700

    update code

--- Day-35/js/main.js
+++ Day-35/js/main.js
@@ -4,27 +4,29 @@ class F8 {
 		componentName = "",
 		{ data = undefined, template = undefined }
 	) {
-		const newData = data();
-		console.log(newData);
 		customElements.define(
 			componentName,
 			class extends HTMLElement {
 				constructor() {
 					super();
-
-					Object.keys(newData).forEach((key) => {
-						window[key] = newData[key];
-					});
 				}
 
 				connectedCallback() {
 					let shadow = this.attachShadow({ mode: "open" });
-					const variablesArr = template.match(/{{.+?}}/g);
-					variablesArr.forEach((variable) => {
-						const variableResult = variable.match(/{{(.+?)}}/)[1].trim();
-						let regex = new RegExp(`{{\\s*${variableResult}\\s*}}`, "g");
-						template = template.replace(regex, window[variableResult]);
-					});
+					if (data) {
+						const newData = data();
+						console.log(newData);
+						Object.keys(newData).forEach((key) => {
+							window[key] = newData[key];
+						});
+
+						const variablesArr = template.match(/{{.+?}}/g);
+						variablesArr.forEach((variable) => {
+							const variableResult = variable.match(/{{(.+?)}}/)[1].trim();
+							let regex = new RegExp(`{{\\s*${variableResult}\\s*}}`, "g");
+							template = template.replace(regex, window[variableResult]);
+						});
+					}
 					shadow.innerHTML = template;
 				}
 			}
@@ -45,9 +47,9 @@ F8.component("counter-app", {
   `,
 });
 
-// F8.component("header-component", {
-// 	template: `<h1>HEADER</h1>`,
-// });
+F8.component("header-component", {
+	template: `<h1>HEADER</h1>`,
+});
 
 // const template = `
 // <p>Hello F8</p>
