675fff3d5dc69106fe38d53de90bc7823bbddec8
duong1801 <duongx1801@gmail.com>
2024-04-08 12:31:35 +0700

    update code

--- Day-36/js/main.js
+++ Day-36/js/main.js
@@ -4,6 +4,8 @@ const btn = document.querySelector(".btn");
 const actionMessage = document.querySelector(".action");
 const messagePending = "Hãy nói nội dung bạn muốn";
 const messageResult = "Đã nói xong. Hy vọng kết quả như ý bạn";
+let resultEl = null;
+let isShowResultVoice = false;
 
 // Tạo một thể hiện mới của SpeechRecognition
 const recognition = new webkitSpeechRecognition();
@@ -16,7 +18,19 @@ recognition.maxAlternatives = 1;
 // Bắt đầu nhận diện khi màn hình được nhấp vào
 btn.onclick = () => {
 	recognition.start();
-	actionMessage.innerText = messagePending;
+	isShowResultVoice = true;
+	let message = actionMessage.innerText;
+	if (!message) {
+		actionMessage.innerText = messagePending;
+	} else {
+		if (actionMessage.classList.contains("success")) {
+			actionMessage.innerText = messagePending;
+			actionMessage.classList.remove("success");
+			if (resultEl) {
+				resultEl.remove();
+			}
+		}
+	}
 };
 
 // Xử lý sự kiện kết quả
@@ -25,73 +39,66 @@ recognition.onresult = (event) => {
 	let targetUrl = "";
 	if (textVoice.includes("facebook")) {
 		targetUrl = "https://www.facebook.com";
-	}
-	// else if (
-	// 	textValue.includes("google") &&
-	// 	!textValue.slice(textValue.indexOf("google") + 7)
-	// ) {
-	// 	if (confirm(`Có phải bạn muốn: ${text} ?`)) {
-	// 		window.open("https://www.google.com", "_blank");
-	// 	}
-	// } else if (textValue.includes("youtube")) {
-	// 	if (confirm(`Có phải bạn muốn: ${text} ?`)) {
-	// 		window.open("https://www.youtube.com", "_blank");
-	// 	}
-	// } else if (textValue.includes("google drive")) {
-	// 	if (confirm(`Có phải bạn muốn: ${text} ?`)) {
-	// 		window.open("https://www.drive.google.com", "_blank");
-	// 	}
-	// } else if (textValue.includes("google map")) {
-	// 	if (confirm(`Có phải bạn muốn: ${text} ?`)) {
-	// 		window.open(
-	// 			"https://www.google.com/maps/@21.0223341,105.8270062,13z?hl=vi-VN&entry=ttu",
-	// 			"_blank"
-	// 		);
-	// 	}
-	// } else if (textValue.includes("bài hát")) {
-	// 	if (confirm(`Có phải bạn muốn: ${text} ?`)) {
-	// 		let songName = textValue.slice(textValue.indexOf("bài hát") + 8);
-	// 		window.open(`https://zingmp3.vn/tim-kiem/tat-ca?q=${songName}`, "_blank");
-	// 	}
-	// } else if (textValue.includes("video")) {
-	// 	if (confirm(`Có phải bạn muốn: ${text} ?`)) {
-	// 		let videoName = textValue.slice(textValue.indexOf("video") + 6);
-	// 		window.open(
-	// 			`https://www.youtube.com/results?search_query=${videoName}`,
-	// 			"_blank"
-	// 		);
-	// 	}
-	// } else if (textValue.includes("tới")) {
-	// 	if (confirm(`Có phải bạn muốn: ${text} ?`)) {
-	// 		let location = textValue.slice(textValue.indexOf("tới") + 4);
-	// 		let url = `https://google.com/maps/search/${location}`;
-	// 		redirect(url);
-	// 	}
-	// } else {
-	// 	if (confirm(`Có phải bạn muốn: ${text} ?`)) {
-	// 		alert(`Không thực hiện được yêu cầu: ${text}`);
-	// 	}
-	// }
+	} else if (textVoice.includes("google")) {
+		targetUrl = "https://www.google.com";
+	} else if (textVoice.includes("youtube")) {
+		targetUrl = "https://www.youtube.com";
+	} else if (textVoice.includes("google drive")) {
+		targetUrl = "https://www.drive.google.com";
+	} else if (textVoice.includes("google map")) {
+		targetUrl =
+			"https://www.google.com/maps/@21.0223341,105.8270062,13z?hl=vi-VN&entry=ttu";
+	} else if (textVoice.includes("bài hát")) {
+		const songName = getInfoByKeyword("bài hát", textVoice);
+		targetUrl = `https://zingmp3.vn/tim-kiem/tat-ca?q=${songName}`;
+	} else if (textVoice.includes("video")) {
+		const videoName = getInfoByKeyword("bài hát", textVoice);
+		targetUrl = `https://zingmp3.vn/tim-kiem/tat-ca?q=${videoName}`;
+	} else if (textVoice.includes("tới") || textVoice.includes("chỉ đường")) {
+		let keyword = "";
+		if (textVoice.includes("chỉ đường tới")) {
+			keyword = "chỉ đường tới";
+		} else if (textVoice.includes("chỉ đường")) {
+			keyword = "chỉ đường";
+		} else if (textVoice.includes("tới")) {
+			keyword = "tới";
+		}
+		let address = getInfoByKeyword(keyword, textVoice);
 
-	showResutVoice(textVoice);
-	if (targetUrl) {
+		targetUrl = `https://google.com/maps/search/${address}`;
 	}
+	resultEl = showResutVoiceAndGetResultEl(textVoice);
+	if (targetUrl) {
+		setTimeout(function () {
+			redirect(targetUrl);
+		}, 1500);
+	} else {
+		setTimeout(function () {
+			const messageCancelRequest = "Không thực hiện được yêu cầu";
+			resultEl.innerText = messageCancelRequest;
+		}, 1500);
+	}
+	isShowResultVoice = false;
 };
 
 function redirect(value) {
 	window.open(value, "_blank");
 }
-function getValue(string, key) {
-	return string.slice(string.indexOf(key) + key.length, string.length - 1);
+function getInfoByKeyword(keyword, string) {
+	let result = string
+		.slice(string.indexOf(keyword) + keyword.length + 1)
+		.trim();
+	return result !== -1 ? result : false;
 }
 
-function showResutVoice(textVoice) {
+function showResutVoiceAndGetResultEl(textVoice) {
 	const div = document.createElement("div");
 	actionMessage.innerText = messageResult;
 	actionMessage.classList.add("success");
 	div.classList.add("result");
 	div.innerText = `Đang thực hiện: ${textVoice}`;
 	searchBox.appendChild(div);
+	return div;
 }
 // Dừng nhận diện khi giọng nói kết thúc
 recognition.onspeechend = () => {
